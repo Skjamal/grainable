ed03bd79cea9171c0dce385ee7ffe4466055630a
Skjamal <satinder.jamal@gmail.com>
2016-05-12 05:04:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grainable::Application.config.secret_key_base = '06c0c8d2bddb4e63985c178688ab2460998f2c84f982f2dfd019057526bbd79451f655b3206791f88343e2d19a8d6f662c2cbf73d3c0ef5e808efe428bf07fbf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grainable::Application.config.session_store :cookie_store, key: '_grainable_session'
